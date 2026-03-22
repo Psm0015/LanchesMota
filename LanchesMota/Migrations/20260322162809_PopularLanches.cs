4d80d9b8d0577952b9e6860888ae97e9d9c9592c
Psm0015 <pedro.ppssmm@gmail.com>
2026-03-22 13:55:02 -0300

    feat: Migration para popular Lanches e Categorias

---
+++ LanchesMota/Migrations/20260322162809_PopularLanches.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMota.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularLanches : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO [dbo].[Lanches]([Nome],[DescricaoCurta],[DescricaoDetalhada],[Preco],[ImagemUrl],[ImagemThumbnailUrl],[IsLanchePreferido],[EmEstoque],[CategoriaId]) VALUES" +
+                "('Chese Salada','Pão, hambúrguer, ovo, presunto, queijo e batata palha','Delicioso pão de hambúrger com ovo frito, presunto e queijo de primeira qualidade acompanhado com batata palha',12.50,'http://www.macoratti.net/Imagens/lanches/cheesesalada1.jpg','http://www.macoratti.net/Imagens/lanches/cheesesalada1.jpg',0,1,1)" +
+                ",('Misto Quente','Pão, presunto, mussarela e tomate','Delicioso pão francês quentinho na chapa com presunto e mussarela bem servidos com tomate preparado com carinho.',8.00,'http://www.macoratti.net/Imagens/lanches/mistoquente4.jpg','http://www.macoratti.net/Imagens/lanches/mistoquente4.jpg',0,1,1)" +
+                ",('Cheese Burger','Pão, hambúrger, presunto, mussarela e batalha palha','Pão de hambúrger especial com hambúrger de nossa preparação e presunto e mussarela; acompanha batata palha.',11.00,'http://www.macoratti.net/Imagens/lanches/cheeseburger1.jpg','http://www.macoratti.net/Imagens/lanches/cheeseburger1.jpg',0,1,1)" +
+                ",('Lanche Natural Peito Peru','Pão Integral, queijo branco, peito de peru, cenoura, alface, iogurte','Pão integral natural com queijo branco, peito de peru e cenoura ralada com alface picado e iorgurte natural.',15.00,'http://www.macoratti.net/Imagens/lanches/lanchenatural.jpg','http://www.macoratti.net/Imagens/lanches/lanchenatural.jpg',1,1,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
